34fa54686ea534d970feb93bb9672bbe5f567c95
gikdev <m.m.bhrm85@gmail.com>
2025-09-30 18:21:27 +0330

    Init

---
+++ steps/steps.ts
@@ -0,0 +1,12 @@
+interface Step {
+  id: string
+}
+
+const steps: Step[] = [
+  { id: "write-the-why" },
+  { id: "what-is-the-internet" },
+  { id: "what-is-a-search-engine" },
+  { id: "what-is-a-web-page" },
+  { id: "what-is-a-web-site" },
+  { id: "what-is-a-web-site" },
+]
